7cbf1254d351cac02dafcef6899435574c51ca24
johncman <johncaulfield374@gmail.com>
2024-07-26 08:59:42 +0000

    Final Commit with description

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 /*
 next term is the sum of the previous two terms.
 The first two terms of the fibonacci sequence are 0 followed by 1.
+The Fibonacci sequence is generated bot recursively and iteratively by the given code. 
+The first terms are initialised in the main function, after which a loop outputs the next eight terms. 
+The Fibonacci number for a particular point is calculated by the recursive function fib, which is then used by another loop to output the first ten terms. 
+This shows how to build the Fibonacci sequence using two different approaches: recursive and iteration. 
 */
 // main() runs in its own thread in the OS
 int t_1 = 0;
